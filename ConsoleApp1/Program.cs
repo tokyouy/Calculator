a51136942a7b3e6c975708d5f861ad02ea901206
tokyouy <tokyouy@DESKTOP-4FLTK79>
2022-01-17 11:27:55 +0900

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double first_num;
+            double second_num;
+            double total;
+            char alert;
+            try
+            {
+                // Первое число
+                Console.WriteLine("Введите первое число");
+                first_num = Convert.ToDouble(Console.ReadLine());
+                // Знак
+                Console.WriteLine("Введите знак");
+                alert = Convert.ToChar(Console.ReadLine());
+                // Второе число
+                Console.WriteLine("Введите второе число");
+                second_num = Convert.ToDouble(Console.ReadLine());
+                if (alert == '+')
+                {
+                    total = first_num + second_num;
+                    Console.WriteLine(total);
+                }
+                else if (alert == '*')
+                {
+                    total = first_num * second_num;
+                    Console.WriteLine(total);
+                }
+                else if (alert == '/')
+                {
+                    total = first_num / second_num;
+                    if (second_num == 0)
+                    {
+                        Console.WriteLine("На ноль делить нельзя");
+                    }
+                    else
+                    {
+                        Console.WriteLine(total);
+                    }
+                }
+                else if (alert == '-')
+                {
+                    total = first_num - second_num;
+                    Console.WriteLine(total);
+                }
+                else
+                {
+                    Console.WriteLine("Неизвестный знак");
+                }
+                Console.ReadLine();
+            }
+            catch (System.Exception)
+            {
+                Console.WriteLine("Что-то было введено не правильно!");
+            }
+            
+        }
+    }
+}
